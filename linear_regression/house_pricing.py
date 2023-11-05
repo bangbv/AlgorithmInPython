cc50bce52e9813c2dec31000d101aa94933dd1c4
bang.bui <vanbang0208@gmail.com>
2023-11-05 16:52:24 +0700

    reformat

--- linear_regression/house_pricing.py
+++ linear_regression/house_pricing.py
@@ -67,41 +67,44 @@ def train_model(model, df, feature, label, epochs, batch_size):
 
 
 print("Defined the create_model and traing_model functions.")
-#@title Define the plotting functions
+
+
+# @title Define the plotting functions
 def plot_the_model(trained_weight, trained_bias, feature, label):
-  """Plot the trained model against 200 random training examples."""
+    """Plot the trained model against 200 random training examples."""
 
-  # Label the axes.
-  plt.xlabel(feature)
-  plt.ylabel(label)
+    # Label the axes.
+    plt.xlabel(feature)
+    plt.ylabel(label)
 
-  # Create a scatter plot from 200 random points of the dataset.
-  random_examples = training_df.sample(n=200)
-  plt.scatter(random_examples[feature], random_examples[label])
+    # Create a scatter plot from 200 random points of the dataset.
+    random_examples = training_df.sample(n=200)
+    plt.scatter(random_examples[feature], random_examples[label])
 
-  # Create a red line representing the model. The red line starts
-  # at coordinates (x0, y0) and ends at coordinates (x1, y1).
-  x0 = 0
-  y0 = trained_bias
-  x1 = 10000
-  y1 = trained_bias + (trained_weight * x1)
-  plt.plot([x0, x1], [y0, y1], c='r')
+    # Create a red line representing the model. The red line starts
+    # at coordinates (x0, y0) and ends at coordinates (x1, y1).
+    x0 = 0
+    y0 = trained_bias
+    x1 = 10000
+    y1 = trained_bias + (trained_weight * x1)
+    plt.plot([x0, x1], [y0, y1], c='r')
 
-  # Render the scatter plot and the red line.
-  plt.show()
+    # Render the scatter plot and the red line.
+    plt.show()
 
 
 def plot_the_loss_curve(epochs, rmse):
-  """Plot a curve of loss vs. epoch."""
+    """Plot a curve of loss vs. epoch."""
 
-  plt.figure()
-  plt.xlabel("Epoch")
-  plt.ylabel("Root Mean Squared Error")
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Root Mean Squared Error")
 
-  plt.plot(epochs, rmse, label="Loss")
-  plt.legend()
-  plt.ylim([rmse.min()*0.97, rmse.max()])
-  plt.show()
+    plt.plot(epochs, rmse, label="Loss")
+    plt.legend()
+    plt.ylim([rmse.min() * 0.97, rmse.max()])
+    plt.show()
+
 
 print("Defined the plot_the_model and plot_the_loss_curve functions.")
 
@@ -112,7 +115,7 @@ batch_size = 30
 
 # Specify the feature and the label.
 my_feature = "total_rooms"  # the total number of rooms on a specific city block.
-my_label="median_house_value" # the median value of a house on a specific city block.
+my_label = "median_house_value"  # the median value of a house on a specific city block.
 # That is, you're going to create a model that predicts house value based
 # solely on total_rooms.
 
@@ -126,7 +129,7 @@ weight, bias, epochs, rmse = train_model(my_model, training_df,
                                          epochs, batch_size)
 
 print("\nThe learned weight for your model is %.4f" % weight)
-print("The learned bias for your model is %.4f\n" % bias )
+print("The learned bias for your model is %.4f\n" % bias)
 
 plot_the_model(weight, bias, my_feature, my_label)
 plot_the_loss_curve(epochs, rmse)
