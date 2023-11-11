744bbda9e6abc3bcab0817221ff5ea384246942f
bang.bui <vanbang0208@gmail.com>
2023-11-11 15:09:15 +0700

    update

--- classification/binary_classification.py
+++ classification/binary_classification.py
@@ -3,12 +3,26 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import layers
 from matplotlib import pyplot as plt
 
-# The following lines adjust the granularity of reporting.
-pd.options.display.max_rows = 10
-pd.options.display.float_format = "{:.1f}".format
-# tf.keras.backend.set_floatx('float32')
 
-print("Ran the import statements.")
+def init_config():
+    # The following lines adjust the granularity of reporting.
+    pd.options.display.max_rows = 10
+    pd.options.display.float_format = "{:.1f}".format
+    # tf.keras.backend.set_floatx('float32')
+
+    print("Ran the import statements.")
+
+
+def load_dataset():
+    train_df = pd.read_csv("~/workspace/AlgorithmInPython/datasets/california_housing_train.csv")
+    test_df = pd.read_csv("~/workspace/AlgorithmInPython/datasets/california_housing_test.csv")
+    train_df = train_df.reindex(np.random.permutation(train_df.index))  # shuffle the training set
+
+
+if __name__ == '__main__':
+    init_config()
+    load_dataset()
+
+
