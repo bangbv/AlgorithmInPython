cc95b6fcaf806a48f5bfc0c03253acc4640faa04
bang.bui <vanbang0208@gmail.com>
2025-06-28 14:53:20 +0700

    add substring and slice

---
+++ basic/string_handle/string_slice.py
@@ -0,0 +1,4 @@
+
+text = "Hello, world! This is a test string."
+print(text[7:12])
+print(text[::-1])  # Reverse the string
---
+++ basic/string_handle/substring_checks.py
@@ -0,0 +1,5 @@
+## Check if a substring exists in a string
+
+text = "The quick brown fox"
+print("quick" in text)  # True
+print("slow" in text)   # False
