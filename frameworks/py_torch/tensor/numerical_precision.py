85870b87e2299068a542edeb061b0282eee2e402
bang.bui <vanbang0208@gmail.com>
2025-10-18 14:40:21 +0700

    numerical precision

---
+++ frameworks/py_torch/tensor/numerical_precision.py
@@ -0,0 +1,9 @@
+import torch
+
+# Creating a tensor with float32 precision
+x = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float32)
+
+# Creating a tensor with float16
+y = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float16)
+
+z = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float64)
