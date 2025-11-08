5a40e6572222597ff5bb751aafd580944de22430
bang.bui <vanbang0208@gmail.com>
2025-11-08 13:56:13 +0700

    add math

--- basic/math/logarithm.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-print("log(1) = ", np.log(1))
-
-print("log(exp(3))", np.log(np.exp(3)))
---
+++ math/logarithm.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print("log(1) = ", np.log(1))
+
+x = np.exp(2) # exp of 3: e^3
+print(f"exponential of 3: {x}")
+print("log(exp(3))", np.log(x))
+
+y = 3 ** 2  # 3 to the power of 2
+print(f"3 to the power of 2: {y}")
+print(f"log of 9: {np.log(y)}")
